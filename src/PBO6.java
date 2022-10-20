89808d82286a87a27ff92bc28f3ae024a115d74a
hussainabdillah <hussainkelarno@gmail.com>
2022-10-20 07:53:18 +0700

    added method this and to show variable

--- src/PBO6.java
+++ src/PBO6.java
@@ -3,5 +3,15 @@ public class PBO6 {
     int umur;
     String alamat;
 
+    public PBO6(String nama, int umur, String alamat) {
+        this.nama = nama;
+        this.umur = umur;
+        this.alamat = alamat;
+    }
 
+    public void tampilkan() {
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Alamat: " + alamat);
+    }
 }
